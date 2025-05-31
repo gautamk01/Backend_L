7d24881c139cba5a8de90e2dca6e1cd638301399
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-05-31 17:42:07 +0530

    First half of the chat application is done , completed the adding user feature , now multiple user can join into a change and list will be updated realtime there is a null problem and chat feature needed to work  more

---
+++ chapter 9 (socket.io)/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+//initiate socket.io and attach this to the http server
+const io = socketIo(server);
+
+//server the static file from the public folder
+app.use(express.static("public"));
+
+const user = new Set(); //store the users
+
+io.on("connection", (socket) => {
+  console.log("A user is now connected ");
+
+  //handle users when they will join the chat
+  //used to listen the emited username
+  socket.on("join", (userName) => {
+    user.add(userName);
+
+    //we have to broadcast to all user that a new user has join ,
+    //all user must needed to know when a user is joined
+
+    io.emit("userJoined", userName);
+
+    //send the updated user list to all clients
+    io.emit("userList", Array.from(user)); //convert the set into array
+  });
+
+  //handle the incomming chat message
+
+  //handle disconnection
+});
+
+server.listen(3000, () => {
+  console.log("server is listening ");
+});
