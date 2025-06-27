d414ac0785b9449b3dcf4a908970d12bda15db96
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-06-27 12:36:28 +0530

    Trying to add Nav bar to my portfolio website

---
+++ GSAP/nav/src/main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import gsap from "gsap";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menuOverlay = document.querySelector(".menu-overlay");
+  const menuContent = document.querySelector(".menu-content");
+  const menuPreviewImg = document.querySelector(".menu-preview-img");
+  const menuLinks = document.querySelectorAll(".link a");
+
+  let isOpen = false;
+});
